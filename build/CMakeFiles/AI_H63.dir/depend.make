9101d15a179d03ff574cd47f72094cbdab5fdc02
AS3E7 <2095439336@qq.com>
2024-09-22 22:23:45 +0800

    add new methods

--- build/CMakeFiles/AI_H63.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for AI_H63.
-# This may be replaced when dependencies are built.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/morgan/ubt/program/demo/ai-engine
+# Install script for directory: /media/ai/AI/technology/computer/ai-engine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/morgan/ubt/program/demo/ai-engine/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/media/ai/AI/technology/computer/ai-engine/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
