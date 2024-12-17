b75fff171d78ee4648261273c4bf600a7f14f9ed
jinyong.yu <jinyong.yu@ubtrobot.com>
2024-12-17 03:00:36 +0000

    update

--- src/module/basic_module.hpp
+++ src/module/basic_module.hpp
@@ -2,30 +2,24 @@
 #ifndef BASIC_MODULE_HPP_
 #define BASIC_MODULE_HPP_
 
-#include "frame.h"
-
 #include <functional>
 #include <memory>
 #include <string>
 
+#include "frame.h"
+
 namespace ai {
 namespace module {
 class BasicModule {
-public:
-  BasicModule(std::string name, int parallelism)
-      : module_name_(name), parallelism_(parallelism) {}
+ public:
+  BasicModule(std::string name, int parallelism) : module_name_(name), parallelism_(parallelism) {}
 
   ~BasicModule() = default;
 
-  void SetProcessDoneCallback(
-      std::function<int(std::shared_ptr<ai::module::Frame>)> callback) {
-    callback_ = callback;
-  }
+  void SetProcessDoneCallback(std::function<int(std::shared_ptr<ai::module::Frame>)> callback) { callback_ = callback; }
 
-  virtual int
-  Transmit(std::shared_ptr<ai::module::Frame> frame) final { // NOLINT
-    if (callback_)
-      return callback_(frame);
+  virtual int Transmit(std::shared_ptr<ai::module::Frame> frame) final {  // NOLINT
+    if (callback_) return callback_(frame);
     return 0;
   }
 
@@ -33,18 +27,18 @@ public:
 
   std::string GetModuleName() { return module_name_; }
 
-public:
+ public:
   virtual int Open() = 0;
   virtual int Process(std::shared_ptr<ai::module::Frame> frame) = 0;
   virtual int Close() = 0;
 
-private:
+ private:
   std::string module_name_ = "";
   int parallelism_ = 1;
   std::function<int(std::shared_ptr<ai::module::Frame>)> callback_;
   std::shared_ptr<BasicModule> next_module_ = nullptr;
 };
 
-} // namespace module
-} // namespace ai
-#endif // BASIC_MODULE_HPP_
+}  // namespace module
+}  // namespace ai
+#endif  // BASIC_MODULE_HPP_
--- src/module/frame.h
+++ src/module/frame.h
@@ -5,6 +5,7 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 #include <vector>
+
 #include "codec/buf_surface_util.hpp"
 
 namespace ai {
@@ -21,42 +22,40 @@ struct DetectObject {
   int label;
   BoundingBox bbox;
   std::string track_id;
-  std::map<std::string, std::string>
-      attributes; // add info into bbox, secondary infer classfication
+  std::map<std::string, std::string> attributes;  // add info into bbox, secondary infer classfication
 };
 
 class Frame {
-public:
+ public:
   int stream_id;
   uint64_t frame_idx;
   bool is_eos;
   std::vector<DetectObject> objs;
   std::string track_id;
-  std::map<std::string, std::string>
-      attributes; // add info into bbox, secondary infer classfication
+  std::map<std::string, std::string> attributes;  // add info into bbox, secondary infer classfication
   cv::Mat img;
   ai::BufSurfWrapperPtr surf;
 };
 
 // Define pixel format enumeration
 enum class PixelFmt {
-    NV12,  // NV12 pixel format
-    // Other pixel formats can be added here
+  NV12,  // NV12 pixel format
+         // Other pixel formats can be added here
 };
 
 // CnFrame structure definition
 struct ai_buffer {
-    uint64_t pts;              // Presentation timestamp
-    int width;                 // Frame width
-    int height;                // Frame height
-    PixelFmt pformat;         // Pixel format
-    uint32_t n_planes;         // Number of planes (e.g., 2 for NV12)
-    uint32_t strides[2];       // Strides for each plane
-    uint32_t frame_size;       // Total frame size (in bytes)
-    uint8_t* ptrs[2];          // Pointers to each plane data
-    uint32_t device_id;        // Device identifier for the frame
+  uint64_t pts;         // Presentation timestamp
+  int width;            // Frame width
+  int height;           // Frame height
+  PixelFmt pformat;     // Pixel format
+  uint32_t n_planes;    // Number of planes (e.g., 2 for NV12)
+  uint32_t strides[2];  // Strides for each plane
+  uint32_t frame_size;  // Total frame size (in bytes)
+  uint8_t* ptrs[2];     // Pointers to each plane data
+  uint32_t device_id;   // Device identifier for the frame
 };
 
-} // namespace module
-} // namespace ai
+}  // namespace module
+}  // namespace ai
 #endif
